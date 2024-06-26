975b6bfeb69e0a629724cc8365623ad706a51e9a
Alan Filho <devaneiosdoalan@gmail.com>
2024-06-26 11:07:58 -0300

    First commit - Comparator

---
+++ Lambda/src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tz", 200.00));
+        list.add(new Product("Lapis", 450.00));
+        list.add(new Product("Alz", 26.00));
+
+        Collections.sort(list);
+
+        for (Product product : list) {
+            System.out.println(product);
+        }
+    }
+}
