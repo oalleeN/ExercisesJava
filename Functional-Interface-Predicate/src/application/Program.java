03ba97355f39f893d0326a08e42f5366a0e0200f
Alan Filho <devaneiosdoalan@gmail.com>
2024-07-01 10:13:51 -0300

    Predicate - Reference method with non-static method

--- Functional-Interface-Predicate/src/application/Program.java
+++ Functional-Interface-Predicate/src/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product::staticProductPredicate); // method reference
+        list.removeIf(Product::nonStaticProductPredicate); // method reference
 
         for (Product p : list) {
             System.out.println(p);
