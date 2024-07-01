d2eafed983de7af83d19524e7bb6cbc3e6389e82
Alan Filho <devaneiosdoalan@gmail.com>
2024-07-01 11:34:38 -0300

    Consumer - Declared lambda expression

--- Functional-Interface-Consumer/src/application/Program.java
+++ Functional-Interface-Consumer/src/application/Program.java
@@ -6,6 +6,7 @@ import util.PriceUpdate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 public class Program {
     public static void main(String[] args) {
@@ -18,7 +19,11 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.forEach(Product::nonStaticPriceUpdate);
+        double factor = 1.1;
+
+        Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
+
+        list.forEach(cons);
 
         list.forEach(System.out::println); // reference method
     }
